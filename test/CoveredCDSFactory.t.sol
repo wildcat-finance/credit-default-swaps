09318f65790fb28a04fbf0bbb56d199aadd373b4
Dr Laurence E. Day <laurence@wildcat.finance>
2026-08-16 04:55:20 +0100

    Support continuous multi-lender cover fills
    Refs #8
    
    Refs #9
    
    Refs #10
    
    Co-authored-by: Shoggoth <shoggoth@wildcat.finance>
    
    Wildcat-Origin: shoggoth

--- test/CoveredCDSFactory.t.sol
+++ test/CoveredCDSFactory.t.sol
@@ -2,9 +2,10 @@
 pragma solidity 0.8.28;
 
 import { CoveredCDSFactory } from "../src/CoveredCDSFactory.sol";
+import { CoveredCDSFacility } from "../src/CoveredCDSFacility.sol";
 import { ExactTransfer } from "../src/libraries/ExactTransfer.sol";
 import { CoveredCDSTestBase } from "./CoveredCDSTestBase.sol";
-import { MockWrapper } from "./mocks/MockTokens.sol";
+import { MockERC20, MockWrapper, MockVault } from "./mocks/MockTokens.sol";
 
 contract CoveredCDSFactoryTest is CoveredCDSTestBase {
   function testCreateBindsCanonicalRegisteredMarketAndEscrowsCollateral() public {
@@ -16,6 +17,8 @@ contract CoveredCDSFactoryTest is CoveredCDSTestBase {
     assertEq(baseAsset.balanceOf(seller), sellerBefore - NOTIONAL);
     assertEq(baseAsset.balanceOf(facility), NOTIONAL);
     assertEq(factory.facilities(0), facility);
+    assertEq(CoveredCDSFacility(facility).referenceShareBudget(), NOTIONAL);
+    assertEq(CoveredCDSFacility(facility).expiry(), block.timestamp + TENOR);
   }
 
   function testRejectsUnregisteredMarket() public {
@@ -67,5 +70,41 @@ contract CoveredCDSFactoryTest is CoveredCDSTestBase {
     vm.prank(seller);
     factory.createFacility(_params());
     assertEq(factory.facilitiesLength(), 0);
+  }
+
+  function testSnapshotsReferenceShareBudgetAndRejectsZeroPreview() public {
+    wrapper.setShareRatio(7, 3);
+    CoveredCDSFacility facility = _create();
+    assertEq(facility.referenceShareBudget(), (NOTIONAL * 7 + 2) / 3);
+
+    wrapper.setShareRatio(0, 1);
+    vm.expectRevert(CoveredCDSFactory.InvalidReferenceShareBudget.selector);
+    vm.prank(seller);
+    factory.createFacility(_params());
+  }
+
+  function testShareBudgetRoundsUpEnoughToWithdrawFullNotional() public {
+    wrapper.setShareRatio(2, 3);
+    CoveredCDSFactory.CreateParams memory params = _params();
+    params.notional = 1;
+    vm.prank(seller);
+    CoveredCDSFacility facility = CoveredCDSFacility(factory.createFacility(params));
+    assertEq(wrapper.previewDeposit(1), 0);
+    assertEq(facility.referenceShareBudget(), 1);
+  }
+
+  function testValidatesOptionalVaultAsset() public {
+    MockERC20 otherAsset = new MockERC20("Other", "OTHER", 6);
+    MockVault wrongVault = new MockVault(address(otherAsset));
+    CoveredCDSFactory.CreateParams memory params = _params();
+    params.collateralVault = address(wrongVault);
+    vm.expectRevert(CoveredCDSFactory.InvalidCollateralVaultAsset.selector);
+    vm.prank(seller);
+    factory.createFacility(params);
+
+    params.collateralVault = address(vault);
+    vm.prank(seller);
+    CoveredCDSFacility facility = CoveredCDSFacility(factory.createFacility(params));
+    assertEq(address(facility.collateralVault()), address(vault));
   }
 }
